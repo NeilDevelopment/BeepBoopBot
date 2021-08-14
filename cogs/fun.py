e7d3ddccfb425acf9b5ee53079b28f28a6b315ac
Neilisop <68216747+Neilisop@users.noreply.github.com>
2021-08-14 15:46:52 +0530

    Server Info command
    Added server information command so users can get information about the server.

--- cogs/fun.py
+++ cogs/fun.py
@@ -91,7 +91,21 @@ class Fun(commands.Cog):
         embed.add_field(name='Top Role?', value=f'{member.top_role}')
         embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles[:1]]))
         embed.set_footer(icon_url=member.avatar_url, text=f'Requested By: {ctx.author.name}')
-        await ctx.send(embed=embed
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases = ['guildinfo', 'Serverinfo', 'ServerInfo', 'GuildInfo', 'Guikldinfo'])
+    async def serverinfo(self, ctx):
+            findbots = sum(1 for member in ctx.guild.members if member.bot)
+            embed = discord.Embed(title = 'Infomation about ' + ctx.guild.name + '.', colour = discord.Colour.from_rgb(54,151,255))
+            embed.set_thumbnail(url = str(ctx.guild.icon_url))
+            embed.add_field(name = "Guild's name: ", value = ctx.guild.name)
+            embed.add_field(name = "Guild's owner: ", value = str(ctx.guild.owner))
+            embed.add_field(name = "Guild's verification level: ", value = str(ctx.guild.verification_level))
+            embed.add_field(name = "Guild's id: ", value = str(ctx.guild.id))
+            embed.add_field(name = "Guild's member count: ", value = str(ctx.guild.member_count))
+            embed.add_field(name="Bots", value=findbots, inline=True)
+            embed.add_field(name = "Guild created at: ", value = str(ctx.guild.created_at.strftime("%a, %d %B %Y, %I:%M %p UTC")))
+            await ctx.send(embed =  embed)
 
 def setup(client):
     client.add_cog(Fun(client))
