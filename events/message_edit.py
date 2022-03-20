eb6f2a63c8df729451598fe340fd59ea5e397d26
unknown <neilrevin3@gmail.com>
2022-03-20 17:13:18 +0530

    Added message edit event
    Signed-off-by: unknown <neilrevin3@gmail.com>

---
+++ events/message_edit.py
@@ -0,0 +1,23 @@
+import discord, os, datetime
+from discord.ext import commands
+from discord.commands import \
+    slash_command, Option
+
+class timeout(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    guild_id = os.getenv("GUILD_ID")
+    log_channel = os.getenv("LOG_CHANNEL")
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, message_before, message_after):
+            embed = discord.Embed(title=f"Message edited in {message_before.channel.name}", description=f"**Before:** {message_before.content}\n**+After:** {message_after.content}", color=discord.Color.blue(), timestamp=datetime.datetime.utcnow())
+            embed.set_author(name=f"{message_before.author.name}", icon_url=f"{message_before.author.avatar.url}")
+            embed.set_footer(text=f"ID: {message_after.author.id}")
+            channel = self.bot.get_channel(int(self.log_channel))
+            await channel.send(embed=embed)
+
+def setup(client):
+    client.add_cog(timeout(client))
