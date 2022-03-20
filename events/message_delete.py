ddd17437fd2be09d0464ac638eb2a8721c8fadb8
unknown <neilrevin3@gmail.com>
2022-03-20 17:12:50 +0530

    Added message delete event
    Signed-off-by: unknown <neilrevin3@gmail.com>

---
+++ events/message_delete.py
@@ -0,0 +1,23 @@
+import discord, os, datetime
+from discord.ext import commands
+from discord.commands import \
+    slash_command, Option
+
+class timeout(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    guild_id = os.getenv("GUILD_ID")
+    log_channel = os.getenv("LOG_CHANNEL")
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        embed = discord.Embed(title=f"Message deleted in {message.channel.name}", description=f"{message.content}", color=0xc25252, timestamp=datetime.datetime.utcnow())
+        embed.set_author(name=f"{message.author.name}", icon_url=f"{message.author.avatar.url}")
+        embed.set_footer(text=f"{message.author.id}")
+        channel = self.bot.get_channel(int(self.log_channel))
+        await channel.send(embed=embed)
+
+def setup(client):
+    client.add_cog(timeout(client))
