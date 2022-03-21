e066fa14dd7c0e2dfbedaa0c83bf838a1491b5e0
unknown <neilrevin3@gmail.com>
2022-03-21 12:53:09 +0530

    Replaced 'avatar.url' with 'avatar'
    Signed-off-by: unknown <neilrevin3@gmail.com>

--- events/message_edit.py
+++ events/message_edit.py
@@ -14,7 +14,7 @@ class message_edit(commands.Cog):
     @commands.Cog.listener()
     async def on_message_edit(self, message_before, message_after):
             embed = discord.Embed(title=f"Message edited in {message_before.channel.name}", description=f"**Before:** {message_before.content}\n**+After:** {message_after.content}", color=discord.Color.blue(), timestamp=datetime.datetime.utcnow())
-            embed.set_author(name=f"{message_before.author.name}", icon_url=f"{message_before.author.avatar.url}")
+            embed.set_author(name=f"{message_before.author.name}", icon_url=f"{message_before.author.avatar}")
             embed.set_footer(text=f"ID: {message_after.author.id}")
             channel = self.client.get_channel(int(self.log_channel))
             await channel.send(embed=embed)
